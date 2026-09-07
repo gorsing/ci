59e2e590ba6f79bc2f2b90004ebe0d1fc1d4a5ae
Martin Kinkelin <noone@nowhere.com>
2026-09-07 16:04:37 +0200

    dstep: Try latest master

--- buildkite/build_project.sh
+++ buildkite/build_project.sh
@@ -54,6 +54,10 @@ case "$REPO_URL" in
         # No tag includes https://github.com/vibe-d/vibe-core/commit/7833e425403c3804054b0bd16eacbf71fc7a28f4
         ref_to_use=master
         ;;
+    https://github.com/jacob-carlborg/dstep)
+        # the latest tag from 2023 doesn't seem to support libclang v18
+        ref_to_use=master
+        ;;
     *)
         ;;
 esac
